0c86a7fc081e308334015ddea0260e5da6f09914
Zuziaxy <raf.zuzia7@gmail.com>
2022-01-04 18:06:40 +0100

    added login form

--- server.js
+++ server.js
@@ -3,7 +3,12 @@ const app = express()
 const PORT = process.env.PORT || 3000; // bardzo istotna linijka - port zostaje przydzielony przez Heroku
 const path = require('path')
 let hbs = require('express-handlebars');
+let data = []
 app.use(express.static('static'))
+
+app.use(express.urlencoded({
+        extended: true
+    }));
 
 app.get("/", function(req, res){
        res.render('mainPage.hbs');
@@ -18,14 +23,34 @@ app.get("/login", function(req, res){
 
 app.get("/admin", function(req, res){
        res.render('admin.hbs');
+  
+});
+
+app.get("/logout", function(req, res){
+       app.engine('hbs', hbs({ defaultLayout: 'main.hbs'}));
+    res.redirect("/")
+  
 });
 
 app.post("/register", function (req, res) {
-        console.log(req.body)
-        res.setHeader("content-type", "text/plain")
-        res.send(req.body)
-    
+    res.send("Zarejestrowano pomyślnie")
+    data.push(req.body)
     })
+
+    app.post("/loginf", function (req, res) {
+        for(let i=0 ; i < data.length;i++){
+            if(req.body.login == data[i].login){
+                if(req.body.password == data[i].password){
+                    res.redirect("/admin")
+                    app.engine('hbs', hbs({ defaultLayout: 'main2.hbs'})); 
+                }else{
+                    res.redirect("/login")
+                }
+            }
+        }
+        })
+
+
 
 app.set('views', path.join(__dirname, 'views'));         // ustalamy katalog views
 app.engine('hbs', hbs({ defaultLayout: 'main.hbs'}));   // domyślny layout, potem można go zmienić
