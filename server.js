9bdf4a5ee21c3a1169c72f67e5500f74598bdb94
Zuziaxy <raf.zuzia7@gmail.com>
2022-01-05 13:20:47 +0100

    added third layout

--- server.js
+++ server.js
@@ -17,58 +17,82 @@ app.use(express.urlencoded({
     }));
 
 app.get("/", function(req, res){
-       res.render('mainPage.hbs');
+    res.render('mainPage.hbs')
+
 });
 
 app.get("/register", function(req, res){
-           res.render('register.hbs');
+         
+    res.render('register.hbs')
+         
 });
 app.get("/login", function(req, res){
-
-       res.render('login.hbs');
+     
+    res.render('login.hbs')
+    
 });
 
 app.get("/admin", function(req, res){
     if(!islogged){
+        app.engine('hbs', hbs({ defaultLayout: 'main.hbs'}));
         res.render('admin.hbs');
     }else{
+        app.engine('hbs', hbs({ defaultLayout: 'main2.hbs'}));
         res.render('adminlogged.hbs');
     }
   
 });
 
 app.get("/show", function(req, res){
-       res.render('show.hbs',{data});
-    console.log(data)
+
+    if(!islogged){
+        res.render('admin.hbs')
+    }else{
+      //  app.engine('hbs', hbs({ defaultLayout: 'main3.hbs'}));
+        res.render('show.hbs',{data,layout:'main3.hbs'});
+    }
+ 
 });
 
 app.get("/gender", function(req, res){
-    let kobiety = []
-    let mezczyzni = []
-    for(let i=0 ; i < data.length;i++){
-        if((data[i].gender == 'female')){
-            kobiety.push(data[i])
-        }else{
-            mezczyzni.push(data[i])
+
+    if(!islogged){
+        res.render('admin.hbs')
+    }else{
+        let kobiety = []
+        let mezczyzni = []
+        for(let i=0 ; i < data.length;i++){
+            if((data[i].gender == 'female')){
+                kobiety.push(data[i])
+            }else{
+                mezczyzni.push(data[i])
+            }
         }
+
+        res.render('gender.hbs',{kobiety,mezczyzni,layout:'main3.hbs'});
     }
-    res.render('gender.hbs',{kobiety,mezczyzni});
-    console.log(data)
+  
 });
 
 app.get("/sort", function(req, res){
-   let dataSorted = data
-   dataSorted.sort(function(a,b){
-        return parseFloat(a.age) - parseFloat(b.age)
-   });
-   res.render('sort.hbs', {dataSorted})
-   console.log(dataSorted)
+    if(!islogged){
+        res.render('admin.hbs',{layout:'main1.hbs'})
+    }else{
+        let dataSorted = data
+        dataSorted.sort(function(a,b){
+             return parseFloat(a.age) - parseFloat(b.age)
+        });
+      // app.engine('hbs', hbs({ defaultLayout: 'main3.hbs'}));
+        res.render('sort.hbs', {dataSorted,layout:'main3.hbs'})
+    }
+  
+
   
 });
 
 
 app.get("/logout", function(req, res){
-       app.engine('hbs', hbs({ defaultLayout: 'main.hbs'}));
+    app.engine('hbs', hbs({ defaultLayout: 'main.hbs'}));
     res.redirect("/")
     islogged = false
   
@@ -123,8 +147,8 @@ app.post("/register", function (req, res) { //formularz rejestracji
                 return parseFloat(a.age) - parseFloat(b.age)
            });
          }
-
-         res.render('sort.hbs', {dataSorted})
+         //app.engine('hbs', hbs({ defaultLayout: 'main3.hbs'}));
+         res.render('sort.hbs', {dataSorted,layout:'main3.hbs'})
     })
 
 
